0ef6192a3f613f0eac7eaf569eb93d8f8afb38ac
DrTroll <gabriel.sthilairev4si972@gmail.com>
2013-10-18 15:43:18 -0400

    Classe de Prise 2

--- src/ca/etsmtl/log121/lab3/de/De.java
+++ src/ca/etsmtl/log121/lab3/de/De.java
@@ -1,19 +1,47 @@
 package ca.etsmtl.log121.lab3.de;
 
 public class De implements Comparable<De> {
-	private  int nbDeFaces;
-    private int valeur;
-    
+	private int nbDeFaces;
+	private int valeur;
+
 	@Override
 	public int compareTo(De autreDe) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	public De(){
-	this.nbDeFaces = 6;
+
+	public De() {
+		this.nbDeFaces = 6;
 	}
-	
-	public void randomize(){
-	this.valeur=(int) Math.floor((Math.random()*6)+1);
+
+	public De(int nbDeFacesRecu) {
+		this.nbDeFaces = nbDeFacesRecu;
+		
 	}
+
+	public De(int nbDeFacesRecu, int valeurRecu) {
+		this(nbDeFacesRecu);
+		this.valeur = valeurRecu;
+	}
+
+	public void randomize() {
+		this.valeur = (int) Math.floor((Math.random() * 6) + 1);
+	}
+
+	public int getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(int valeurRecu) {
+		this.valeur = valeurRecu;
+	}
+
+	public int getnbDeFaces() {
+		return nbDeFaces;
+	}
+
+	public void setnbDeFaces(int nbDeFacesRecu) {
+		this.nbDeFaces = nbDeFacesRecu;
+	}
+
 }
