6ef54d0eeae6869da5959624aacd83e50a29b8d2
Frédéric Gascon <frederic.gascon@gmail.com>
2013-10-18 15:51:25 -0400

    CollectionJoueurs

--- src/ca/etsmtl/log121/lab3/joueur/CollectionJoueurs.java
+++ src/ca/etsmtl/log121/lab3/joueur/CollectionJoueurs.java
@@ -1,8 +1,20 @@
 package ca.etsmtl.log121.lab3.joueur;
 
+import java.util.ArrayList;
+
 import ca.etsmtl.log121.lab3.Iterable;
 
 public class CollectionJoueurs implements Iterable<Joueur> {
+
+	private ArrayList<Joueur> list;
+
+	public CollectionJoueurs() {
+		list = new ArrayList<Joueur>();
+	}
+
+	public void ajouterJoueur(Joueur joueur) {
+		list.add(joueur);
+	}
 
 	@Override
 	public IterateurJoueur creerIterateur() {
