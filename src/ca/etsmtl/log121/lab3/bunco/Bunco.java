736e3d67f5511b498cb560a3cee499c179b6f005
Frédéric Gascon <frederic.gascon@gmail.com>
2013-10-18 15:28:07 -0400

    stub de jeux bunco

---
+++ src/ca/etsmtl/log121/lab3/bunco/Bunco.java
@@ -0,0 +1,27 @@
+package ca.etsmtl.log121.lab3.bunco;
+
+import ca.etsmtl.log121.lab3.IStrategie;
+import ca.etsmtl.log121.lab3.Jeux;
+import ca.etsmtl.log121.lab3.joueur.Joueur;
+
+public class Bunco implements IStrategie {
+
+	@Override
+	public void initialiserJeux(Jeux jeux) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Joueur calculerLeVainqueur(Jeux jeux) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int calculerScoreTour(Jeux jeux) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
