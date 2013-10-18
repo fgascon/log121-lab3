69298bcb4334146dec218171b4aed203fd335ce6
DrTroll <gabriel.sthilairev4si972@gmail.com>
2013-10-18 15:23:34 -0400

    Classe dé
    Méthode

--- src/ca/etsmtl/log121/lab3/de/De.java
+++ src/ca/etsmtl/log121/lab3/de/De.java
@@ -1,11 +1,19 @@
 package ca.etsmtl.log121.lab3.de;
 
 public class De implements Comparable<De> {
-
+	private  int nbDeFaces;
+    private int valeur;
+    
 	@Override
 	public int compareTo(De autreDe) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	public De(){
+	this.nbDeFaces = 6;
+	}
+	
+	public void randomize(){
+	this.valeur=(int) Math.floor((Math.random()*6)+1);
+	}
 }
