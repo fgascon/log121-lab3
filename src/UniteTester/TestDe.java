d55802985ff52f77bcf12a7536baab63bd50066b
DrTroll <gabriel.sthilairev4si972@gmail.com>
2013-11-01 16:03:04 -0400

    Joueur test junit

--- src/UniteTester/TestDe.java
+++ src/UniteTester/TestDe.java
@@ -1,5 +1,18 @@
 package UniteTester;
 
-public class TestDe {
 
+import org.junit.*;
+import static org.junit.Assert.*;
+import ca.etsmtl.log121.lab3.IStrategie;
+import ca.etsmtl.log121.lab3.Joueur;
+
+
+
+public class TestDe {
+	
+	@Test
+	public void testcossin() {
+		assertTrue(false);
+	}
+	
 }
